8feff494b60a3b06701a3c98272d3645848639e0
Matti Lamppu <lamppu.matti.akseli@gmail.com>
2024-09-19 18:43:54 +0300

    WIP

--- fast_forward.py
+++ fast_forward.py
@@ -14,6 +14,9 @@ from schema import IssueCommentEvent
 
 
 def main(*, github_token: str, event_path: str) -> int:
+    print("github_token:", github_token)
+    print("event_path:", event_path)
+
     with open(event_path, encoding="utf-8") as f:
         data: IssueCommentEvent = json.load(f)
 
