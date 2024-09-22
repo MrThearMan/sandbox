c75e753cded39a560078924757de305b4aeba8f0
Matti Lamppu <lamppu.matti.akseli@gmail.com>
2024-09-22 19:06:55 +0300

    WIP

--- prff/fast_forward.py
+++ prff/fast_forward.py
@@ -1,14 +1,6 @@
 from __future__ import annotations
 
-from prff.git import (
-    approve_git_credentials,
-    clone_repo_at_ref,
-    create_branch,
-    fetch_ref,
-    push_branch_to_ref,
-    set_git_credential_helper_to_store,
-    validate_fast_forward,
-)
+from prff.git import clone_repo_at_ref, create_branch, fetch_ref, push_branch_to_ref, validate_fast_forward
 from prff.permissions import fetch_user_permissions, validate_push_permissions
 from prff.pull_request import PullRequestData
 
@@ -22,11 +14,6 @@ def fast_forward_pull_request(*, pull_request_url: str, permissions_url: str) ->
     validate_push_permissions(permissions=permissions)
 
     pull_request = PullRequestData.from_github(url=pull_request_url)
-
-    set_git_credential_helper_to_store()
-    approve_git_credentials(repo_url=pull_request.base_clone_url)
-    if pull_request.pr_clone_url != pull_request.base_clone_url:
-        approve_git_credentials(repo_url=pull_request.pr_clone_url)
 
     clone_repo_at_ref(repo_url=pull_request.auth_base_clone_url, branch_name=pull_request.base_branch_name)
     fetch_ref(repo_url=pull_request.auth_pr_clone_url, commit_sha=pull_request.pr_head_sha)
